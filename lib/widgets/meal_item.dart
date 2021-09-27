c38655663e5e43a6999623a5bfe82f8652599834
MostafaAmin0 <mostafa.aminmk@gmail.com>
2021-09-27 14:19:31 +0200

    positionning text on image

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -30,6 +30,24 @@ class MealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 5,
+                  child: Container(
+                    width: 300,
+                    padding: const EdgeInsets.symmetric(vertical: 5,horizontal: 20),
+                    color: Colors.black54,
+                    child: Text(
+                      meal.title,
+                      style:const TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 )
               ],
             ),
